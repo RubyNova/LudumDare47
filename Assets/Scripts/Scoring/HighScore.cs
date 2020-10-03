02be0500e49ff8f8f545ccb8fa3b8a16ca77f936
Ian <barrowian1991@gmail.com>
2020-10-03 19:07:08 +0100

    Added high score saving with player prefs, display current and high score, main menu display high score

--- Assets/Scripts/Scoring/HighScore.cs
+++ Assets/Scripts/Scoring/HighScore.cs
@@ -1,15 +1,27 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Scoring
 {
     public class HighScore : MonoBehaviour
     {
         [SerializeField] private int _currentScore;
+        [SerializeField] private Text _scoreText;
+        [SerializeField] private Text _highscoreText;
 
         public int CurrentScore
         {
             get => _currentScore;
             set => _currentScore = value;
+        }
+
+        private void Update()
+        {
+            if (_scoreText) _scoreText.text = "Score: " + _currentScore;
+            if (_highscoreText) _highscoreText.text = "High Score: " + PlayerPrefs.GetFloat("score");
+            if (!(PlayerPrefs.GetFloat("score", 0) <= _currentScore)) return;
+            PlayerPrefs.SetFloat("score", _currentScore);
         }
     }
 }
