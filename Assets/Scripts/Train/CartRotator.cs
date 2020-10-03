6dd7015847e3d022d7353ada0ae86dab2a7cdee8
Matt Jones <matthewj866@gmail.com>
2020-10-03 14:12:34 +0100

    Added rotating cart behaviour so the cart animates correctly.

---
+++ Assets/Scripts/Train/CartRotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartRotator : MonoBehaviour
+{
+    [SerializeField] private Transform _positionToLookAt;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dir = (transform.position - _positionToLookAt.position).normalized;
+        float distance = Vector3.Distance(transform.position, _positionToLookAt.position);
+        var newRightPos = _positionToLookAt.position + (dir * (distance * 2));
+        transform.right = newRightPos;
+    }
+}
