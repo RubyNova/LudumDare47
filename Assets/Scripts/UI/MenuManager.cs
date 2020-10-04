884edda45034df89415374d776bcf79fb2305c84
Ian <barrowian1991@gmail.com>
2020-10-04 16:44:21 +0100

    Added menu manager

---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace UI
+{
+    public class MenuManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] _menuObjects;
+        [SerializeField] private bool[] _menuObjectState;
+
+        private void OnValidate()
+        {
+            if (_menuObjectState.Length != _menuObjects.Length) _menuObjectState = new bool[_menuObjects.Length];
+            for (int i = 0; i < _menuObjects.Length; i++)
+            {
+                _menuObjects[i].SetActive(_menuObjectState[i]);
+            }
+        }
+    }
+}
