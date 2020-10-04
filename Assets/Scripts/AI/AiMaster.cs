5e8855e35bbc4d476277e4ad8c202b3ace9b70db
Ian <barrowian1991@gmail.com>
2020-10-04 14:53:59 +0100

    Created enemy animation

--- Assets/Scripts/AI/AiMaster.cs
+++ Assets/Scripts/AI/AiMaster.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Train;
+using UnityEditor.Animations;
 using UnityEngine;
 
 namespace AI
@@ -11,12 +12,16 @@ namespace AI
         [SerializeField] protected float _attackDamage;
         [SerializeField] protected int _health;
         [SerializeField] protected int _pointValue;
+        [SerializeField] protected Animator _animator;
+        [SerializeField] protected float _trackOffset;
+        private readonly int _walkAnimation = Animator.StringToHash("Walking");
+        private readonly int _attackAnimation = Animator.StringToHash("Attacking");
         protected float _attackTimer = 0;
         protected AiState _currentState = AiState.Spawned;
         protected TrackHealth _trackHealth;
         
-        public AiSpawner _parentSpawn { get; set; }
-        
+        public AiSpawner ParentSpawn { get; set; }
+
         public float Distance { get; set; }
 
         public void Update()
@@ -25,14 +30,20 @@ namespace AI
             {
                 case AiState.Moving:
                     Moving();
+                    _animator.SetBool(_walkAnimation, true);
+                    _animator.SetBool(_attackAnimation, false);
                     break;
                 case AiState.Attacking:
                     Attacking();
+                    _animator.SetBool(_walkAnimation, false);
+                    _animator.SetBool(_attackAnimation, true);
                     break;
                 case AiState.Spawned:
                     if (Math.Abs(Distance) > 0) _currentState = AiState.Moving;
                     break;
                 case AiState.Celebrating:
+                    _animator.SetBool(_walkAnimation, false);
+                    _animator.SetBool(_attackAnimation, false);
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
